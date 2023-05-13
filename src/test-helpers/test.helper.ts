e9bc03401d0831d5c9298cbb5491e6a573447103
Somnath Sinha <som30ind@gmail.com>
2023-05-13 23:26:11 +0530

    Integrated Jest Testing and Codcov Coverage CI

---
+++ src/test-helpers/test.helper.ts
@@ -0,0 +1,16 @@
+import { DurationSpecKeys } from '../types/duration.type';
+
+const DUR_SPECS: Record<DurationSpecKeys | 'mcr' | 'ns', number> = {
+  'ns': 0,
+  'mcr': 1e3,
+  'ms': 1e6,
+  'secs': 1e9,
+  'mins': 60 * 1e9,
+  'hrs': 60 * 60 * 1e9,
+  'days': 24 * 60 * 60 * 1e9
+};
+
+export function toNanoSecond(days: number, hours: number, mins: number, secs: number, milli: number, micro: number, nano: number): number {
+  return (days * DUR_SPECS.days) + (hours * DUR_SPECS.hrs) + (mins * DUR_SPECS.mins)
+    + (secs * DUR_SPECS.secs) + (milli * DUR_SPECS.ms) + (micro * DUR_SPECS.mcr) + (nano * DUR_SPECS.ns);
+}
